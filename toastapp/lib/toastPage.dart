c2cca2151f3fc9e135f373c2d0adf71b122d36e6
TIENTCHEUisrael <tientcheuisrael@gmail.com>
2024-02-03 20:37:17 +0100

    style modified

--- toastapp/lib/toastPage.dart
+++ toastapp/lib/toastPage.dart
@@ -12,40 +12,49 @@ class _ToastPageState extends State<ToastPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.symmetric(horizontal:50),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green
-              ),
-              child: const Text('Click there!'),
-              onPressed: (){
-                Fluttertoast.showToast(
-                    msg: 'I am TILStack. This is a Toast',
-                    textColor: Colors.white,
-                    backgroundColor: Colors.black,
-                    gravity: ToastGravity.BOTTOM,
-                    fontSize: 16.0,
-                    timeInSecForIosWeb: 1,
-                    toastLength: Toast.LENGTH_SHORT,
-
-                );
-              },
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal:50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.green
+                ),
+                child: const Padding(
+                  padding:  EdgeInsets.all(20.0),
+                  child: Text('Click there!',style: TextStyle(color: Colors.white),),
+                ),
+                onPressed: (){
+                  Fluttertoast.showToast(
+                      msg: 'I am TILStack. This is a Toast',
+                      textColor: Colors.black,
+                      backgroundColor: Colors.white,
+                      gravity: ToastGravity.BOTTOM,
+                      fontSize: 16.0,
+                      timeInSecForIosWeb: 1,
+                      toastLength: Toast.LENGTH_SHORT,
+
+                  );
+                },
               ),
-              child: const Text('Cancel there!'),
-              onPressed: (){
-                Fluttertoast.cancel();
-              },
-            ),
-          ],
+              const SizedBox(height: 20,),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text('Cancel there!',style: TextStyle(color: Colors.white),),
+                ),
+                onPressed: (){
+                  Fluttertoast.cancel();
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
